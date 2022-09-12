0307b14e1eef483559fa1eb1a6cb2663fad702b9
Florian Schieder <florian.schieder@puzzleyou.de>
2022-09-12 15:38:58 +0200

    __version__ in parviraptor/__init__.py setzen
    Sonst weigert sich django-app-helper die Tests für `parviraptor`
    auszuführen.

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import find_packages, setup
 
+from parviraptor import __version__
+
 setup(
     name="parviraptor",
-    version="0.1.0",
+    version=__version__,
     description="Django-based job queue",
     author="puzzleYOU GmbH",
     packages=find_packages("."),
