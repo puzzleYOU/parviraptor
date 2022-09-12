1540e2af296eb7b8984084b9bfba294121ca9087
Florian Schieder <florian.schieder@puzzleyou.de>
2022-09-12 15:40:17 +0200

    Testumgebung angelegt
    - Einstiegspunkt: `devtools/run-tests`
      - fährt einen MySQL-Server hoch
      - baut ein Image, auf dessen Basis die Unittests ausgeführt werden
        können
      - liefert im Erfolgsfall Exit Code 0, sonst 1.

---
+++ parviraptor/tests/settings.py
@@ -0,0 +1,11 @@
+HELPER_SETTINGS = {
+    "DATABASES": {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "HOST": "mysql",
+            "NAME": "parviraptor",
+            "PASSWORD": "dev",
+            "USER": "parviraptor",
+        },
+    },
+}
