6c0a98637093fee8a619b35053212100ded80612
Florian Schieder <florian.schieder@puzzleyou.de>
2022-09-13 07:50:18 +0200

    `monitor_queue_entries()` besser dokumentiert

--- parviraptor/monitoring.py
+++ parviraptor/monitoring.py
@@ -15,6 +15,28 @@ def monitor_queue_entries(
     job_classes: Iterable[type],
 ) -> List[QueueMonitoringResult]:
 
+    """
+    Nimmt eine Sammlung von Job-Klassen entgegen und liefert eine Liste von
+    `QueueMonitoringResult`s zurück.
+
+    Ein `QueueMonitoringResult` für die übergebene Job-Klasse taucht nur
+    dann im Rückgabewert auf, wenn ein Job fehlgeschlagen oder potentiell
+    hängengeblieben ist.
+
+    - Wird bspw. `[DummyJob]` übergeben und es ist kein Job auf `FAILED` oder
+      zu lange auf `PROCESSING`, wird `[]` zurückgegeben.
+
+    - Wird bspw. `[DummyJob]` übergeben und es ist 1 Job auf `FAILED` und
+      keiner zu lange auf `PROCESSING`, befindet sich in der Rückgabeliste
+      ein Element mit `queue_name = "DummyJob"`, `failed_jobs_count = 1`
+      und `long_processing_jobs_count = 0`.
+
+    - Wird bspw. `[DummyJob]` übergeben und es ist kein Job auf `FAILED` und
+      1 Job zu lange auf `PROCESSING`, befindet sich in der Rückgabeliste
+      ein Element mit `queue_name = "DummyJob"`, `failed_jobs_count = 0`
+      und `long_processing_jobs_count = 1`.
+    """
+
     mistyped_classes = list(
         filter(
             lambda job_class: not issubclass(job_class, AbstractJob),
